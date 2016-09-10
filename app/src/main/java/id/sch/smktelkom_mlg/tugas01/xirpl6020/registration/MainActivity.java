39620967fe4f1f24159c02e1d9eb86a9a976f2e0
Dafa Zakhulhaq <dafa27890@gmail.com>
2016-09-10 10:38:46 +0700

    Third

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl6020/registration/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl6020/registration/MainActivity.java
@@ -6,16 +6,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText nama, tahun;
-    TextView tvhasil;
+    TextView tvhasil, tvhasil2, tvhasil3, tvhasil4;
     Button sign;
     RadioGroup rg;
     CheckBox rpl, tkj, multi;
+    Spinner kota;
 
 
     @Override
@@ -30,12 +33,21 @@ public class MainActivity extends AppCompatActivity {
         rpl = (CheckBox) findViewById(R.id.checkBox);
         tkj = (CheckBox) findViewById(R.id.checkBox2);
         multi = (CheckBox) findViewById(R.id.checkBox3);
+        tvhasil2 = (TextView) findViewById(R.id.hasil2);
+        tvhasil3 = (TextView) findViewById(R.id.hasil3);
+        tvhasil4 = (TextView) findViewById(R.id.hasil4);
+        kota = (Spinner) findViewById(R.id.spinner);
+
+
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 doklik();
+                doradio();
+                docheck();
+                dospin();
 
             }
         });
@@ -43,12 +55,107 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void doklik() {
+    private void dospin() {
 
-        String nama1 = nama.getText().toString();
-        int lahir = Integer.parseInt(tahun.getText().toString());
-        int umur = 2016 - lahir;
-        tvhasil.setText("" + nama1 + "/n" + umur);
+        tvhasil4.setText(" Kota Asal = " + kota.getSelectedItem().toString());
 
     }
+
+    private void docheck() {
+
+        String hasil = " Jurusan Anda = ";
+        int starlen = hasil.length();
+        if (rpl.isChecked()) hasil += rpl.getText() + ",";
+        if (tkj.isChecked()) hasil += tkj.getText() + ",";
+        if (multi.isChecked()) hasil += multi.getText() + ",";
+
+        if (hasil.length() == starlen) hasil += " Nada Belum Memilih Jurusan";
+
+        tvhasil3.setText(hasil);
+
+    }
+
+    private void doradio() {
+
+        String hasil = null;
+
+        if (rg.getCheckedRadioButtonId() != -1) {
+
+            RadioButton rb = (RadioButton) findViewById(rg.getCheckedRadioButtonId());
+            hasil = rb.getText().toString();
+
+        } else {
+
+
+        }
+
+        if (hasil == null) {
+
+            tvhasil2.setText(" Anda Belum Memilih Kelamin ");
+
+        } else {
+
+            tvhasil2.setText(" Kelamin Anda = " + hasil);
+
+        }
+
+
+    }
+
+    private void doklik() {
+
+
+        if (isvalid()) {
+
+            String nama1 = nama.getText().toString();
+            int lahir = Integer.parseInt(tahun.getText().toString());
+            int umur = 2016 - lahir;
+            tvhasil.setText(" Nama = " + nama1 + "\n Umur = " + umur);
+
+        }
+
+    }
+
+    private boolean isvalid() {
+
+        boolean valid = true;
+
+        valid = true;
+
+        String namav = nama.getText().toString();
+        String tahunv = tahun.getText().toString();
+
+        if (namav.isEmpty()) {
+
+            nama.setError("Nama Belum Diisi");
+            valid = false;
+
+        } else if (namav.length() < 3) {
+
+            nama.setError("Minimal 3 Karakter");
+            valid = false;
+        } else {
+
+            nama.setError(null);
+
+        }
+
+        if (tahunv.isEmpty()) {
+
+            tahun.setError("Tahun Belum Diisi");
+            valid = false;
+        } else if (tahun.length() != 4) {
+
+            tahun.setError("Format yyyy");
+            valid = false;
+        } else {
+
+            tahun.setError(null);
+        }
+
+
+        return valid;
+    }
+
+
 }
